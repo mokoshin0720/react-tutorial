2a6d99a888fb65c7ab697f8f2e6a1a2f667d855b
mokoshin0720 <shinya.work0720@gmail.com>
2021-08-08 14:58:04 +0900

    [add]save state in board

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -4,18 +4,12 @@ import './index.css';
 
 // 正方形のマス目
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
+        // マス目をクリックしたら、Squareのvalueを'X'にする
         <button className="square"
-                onClick={() => this.setState({value: 'X'})}>
-            {this.state.value}
+                onClick={() => this.props.onClick()}>
+            {this.props.value}
         </button>
         );
     }
@@ -23,8 +17,25 @@ class Square extends React.Component {
 
 // 盤面
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+        <Square value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+        />
+        );
     }
 
     render() {
