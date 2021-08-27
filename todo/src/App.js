1a47c6b5e2331ebb4a452f784ac528eeab30e7b9
mokoshin0720 <shinya.work0720@gmail.com>
2021-08-27 19:07:21 +0900

    [add]props and state

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,6 +1,19 @@
+import { useState } from "react";
+import { List } from "./List";
+
 function App() {
+  const [description, setDescription] = useState('クリック前の表示');
+
+  const changeDescription = () => {
+    setDescription('クリック後の表示');
+  }
+
   return (
-    <div>ゼロから始めるReact入門</div>
+    <div>
+      { description }
+      <List title="取り扱い言語一覧"/>
+      <button onClick={changeDescription}>変更する</button>
+      </div>
   );
 }
 
