dea56d10864ceb9853b226da511b1451d71bc243
mokoshin0720 <shinya.work0720@gmail.com>
2021-08-28 12:40:38 +0900

    [add]HOC

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { List } from "./List";
 import { Form } from "./Form";
 import { getLanguages } from "./const/languages";
+import { withLoading } from './hoc/withLoading';
 
 const Header = styled.header`
   display: flex;
@@ -24,18 +25,9 @@ const HeaderLi = styled.li`
   border-bottom: ${props => props.focused ? '2px solid #F44336': 'none'}
 `
 
-function App() {
+function App({ data }) {
   const [tab, setTab] = useState('list')
-  const [langs, setLangs] = useState([])
-
-  useEffect(() => {
-    fetchLanguages();
-  }, [])
-
-  const fetchLanguages = async () => {
-    const languages = await getLanguages();
-    setLangs(languages)
-  }
+  const [langs, setLangs] = useState(data)
 
   const addLang = (lang) => {
     setLangs([...langs, lang]);
@@ -58,4 +50,4 @@ function App() {
   );
 }
 
-export default App;
+export default withLoading(App, getLanguages);
