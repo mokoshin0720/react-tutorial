f4cbf3acd1c196d49818b02b0eb4cc7c0de6c214
mokoshin0720 <shinya.work0720@gmail.com>
2021-10-02 16:01:43 +0900

    [add]useState

--- book/super_basic_react_and_next/react_app/src/App.js
+++ book/super_basic_react_and_next/react_app/src/App.js
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import './App.css';
-import Rect from './Rect';
 
-function App() {
+function App(props) {
+  const [msg, setMsg] = useState("Hello Component");
+  const [count, setCount] = useState(0);
+
+  const change = () => {
+    setMsg("Hello")
+    setCount(count+1)
+  }
+
   return (
     <div className="App">
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
-        <p className="subtitle">draw rectangle.</p>
-        <Rect x="200" y="200" w="200" h="200" c="#6ff9" r="25" />
-        <Rect x="300" y="300" w="200" h="200" c="#f6f9" r="75" />
-        <Rect x="400" y="400" w="200" h="200" c="#6669" r="100" />
+        <p className="subtitle">Show message.</p>
+        <p className="alert alert-warning">{props.msg}</p>
+        <p className="alert alert-dark">{msg}, {count}</p>
+        <button onClick={change}>change</button>
       </div>
     </div>
   );
