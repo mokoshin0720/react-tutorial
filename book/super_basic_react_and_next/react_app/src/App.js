28d3a508e38f820282bfb133778d5871562a1a38
mokoshin0720 <shinya.work0720@gmail.com>
2021-10-02 15:26:36 +0900

    [add]initialize react_app

--- book/super_basic_react_and_next/react_app/src/App.js
+++ book/super_basic_react_and_next/react_app/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="bg-primary text-white display-4">React</h1>
+      <div className="container">
+        <p className="subtitle">This is sample component.</p>
+        <p>display short message</p>
+      </div>
     </div>
   );
 }
