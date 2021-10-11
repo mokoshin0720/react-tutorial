c24e2d97ee9b50f53360b5e12e96c16dd75d5ff5
mokoshin0720 <shinya.work0720@gmail.com>
2021-10-11 19:53:44 +0900

    [add]use useEffect

--- book/super_basic_react_and_next/react_app/src/App.js
+++ book/super_basic_react_and_next/react_app/src/App.js
@@ -1,70 +1,41 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
-  const [name, setName] = useState("")
-  const [mail, setMail] = useState("")
-  const [age, setAge] = useState(0)
-  const [form, setForm] = useState({
-    name: "no name",
-    mail: "no mail",
-    age: 0
-  })
+  const [val, setVal] = useState(0)
+  const [msg, setMsg] = useState("set a number...")
 
-  const doChangeName = (event) => {
-    setName(event.target.value)
+  const doChange = (event) => {
+    setVal(event.target.value)
   }
-  const doChangeMail = (event) => {
-    setMail(event.target.value)
-  }
-  const doChangeAge = (event) => {
-    setAge(event.target.value)
-  }
-  const doSubmit = (event) => {
-    setForm({name: name, mail: mail, age: age})
-    event.preventDefault()
-  }
+
+  useEffect(() => {
+    let total = 0
+    for  (let i=0; i<=val; i++) {
+      total += i
+    }
+    setMsg("total: " + total)
+  })
 
   return (
     <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
-        <AlertMessage data={form} setData={setForm} />
-        <form onSubmit={doSubmit}>
-          <div className="form-group">
-            <label>Name: </label>
-            <input type="text" className="form-control" onChange={doChangeName} />
-          </div>
-          <div className="form-group">
-            <label>Mail: </label>
-            <input type="text" className="form-control" onChange={doChangeMail} />
-          </div>
-          <div className="form-group">
-            <label>Age: </label>
-            <input type="text" className="form-control" onChange={doChangeAge} />
-          </div>
-          <input type="submit" className="btn btn-primary" value="Click" />
-        </form>
+        <AlertMessage msg={msg} />
+        <div className="form-group">
+          <label>Input: </label>
+          <input type="number" className="form-control" onChange={doChange} />
+        </div>
       </div>
     </div>
   )
 }
 
 const AlertMessage = (props) => {
-  const data = props.data
-  const msg = JSON.stringify(props.data)
-
   return (
     <div className="alert alert-primary h5 text-primary">
-      <h5>{msg}</h5>
-      <table className="table h6">
-        <tbody>
-          <tr><th>Name</th><td>{data.name}</td></tr>
-          <tr><th>Mail</th><td>{data.mail}</td></tr>
-          <tr><th>Age</th><td>{data.age}</td></tr>
-        </tbody>
-      </table>
+      <h5>{props.msg}</h5>
     </div>
   )
 }
