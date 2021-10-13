039c4efd56e3fb8f5dd821cafb0b53f474290cec
mokoshin0720 <shinya.work0720@gmail.com>
2021-10-13 17:12:14 +0900

    [add]use reused hooks

--- book/super_basic_react_and_next/react_app/src/App.js
+++ book/super_basic_react_and_next/react_app/src/App.js
@@ -1,34 +1,90 @@
 import './App.css';
 import React, { useState } from 'react';
 
-const useCounter = () => {
-  const [num, setNum] = useState(0)
-
-  const count = () => {
-    setNum(num+1)
+// 合計を計算する関数
+const total = (a) => {
+  let re = 0
+  for (let i=0; i<=a; i++) {
+    re += i
   }
-
-  return [num, count]
+  return re
 }
 
-const AlertMessage = () => {
-  const [counter, plus] = useCounter()
+// 消費税を計算する関数
+const tax = (a) => {
+  return Math.floor(a * 1.1)
+}
+
+// 数値を計算しメッセージを返す独自フック
+const useCalc = (num = 0, func=(a)=>{return a}) => {
+  const [msg, setMsg] = useState(null)
+
+  const setValue = (p) => {
+    let res = func(p)
+    setMsg(<p className="h5"> * {p}の結果は、{res}です。</p>)
+  }
+
+  return [msg, setValue]
+}
+
+// デフォルトのコンポーネント
+const PlainMessage = () => {
+  const [msg, setCalc] = useCalc()
+
+  const onChange = (e) => {
+    setCalc(e.target.value)
+  }
 
   return (
-    <div className="alert alert-primary h5 text-center">
-      <h4>count: {counter} .</h4>
-      <button onClick={plus} className="btn btn-primary">count</button>
+    <div className="p-3 h5">
+      <h5>{msg}</h5>
+      <input type="number" onChange={onChange} className="form-control" />
     </div>
   )
 }
 
+// 合計を計算するコンポーネント
+const AlertMessage = () => {
+  const [msg, setCalc] = useCalc(0, total)
+
+  const onChange = (e) => {
+    setCalc(e.target.value)
+  }
+
+  return (
+    <div className="alert alert-primary h5 text-primary">
+      <h5>{msg}</h5>
+      <input type="number" onChange={onChange} min="0" max="10000" className="form-control" />
+    </div>
+  )
+}
+
+// 消費税を計算するコンポーネント
+const CardMessage = (props) => {
+  const [msg, setCalc] = useCalc(0, tax)
+
+  const onChange = (e) => {
+    setCalc(e.target.value)
+  }
+
+  return (
+    <div className="card p-3 h5 border-primary">
+      <h5>{msg}</h5>
+      <input type="range" onChange={onChange} min="0" max="10000" step="100" className="form-control" />
+    </div>
+  )
+}
+
+// ベースのコンポーネント
 const App = () => {
   return (
     <div>
       <h1 className="bg-primary text-white display-4">React</h1>
       <div className="container">
         <h4 className="my-3">Hooks sample</h4>
+        <PlainMessage />
         <AlertMessage />
+        <CardMessage />
       </div>
     </div>
   )
