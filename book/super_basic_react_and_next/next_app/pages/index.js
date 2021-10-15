4d62450315a11f9c317c8ab8d7d758964fe08b3d
mokoshin0720 <shinya.work0720@gmail.com>
2021-10-15 18:35:11 +0900

    [add]fetch json

--- book/super_basic_react_and_next/next_app/pages/index.js
+++ book/super_basic_react_and_next/next_app/pages/index.js
@@ -1,19 +1,35 @@
-import Header from "./header"
-import Link from "next/link"
-import Image from "next/image"
 import Layout from "../components/layout"
-import MyImage from "../components/image"
+import useSWR from "swr"
 
 export default function Home() {
+  const func = (...args) => fetch(...args).then(res => res.json())
+  const {data} = useSWR('/data.json', func)
+
   return (
     <div>
       <Layout header="Next.js" title="Top page">
         <div className="alert alert-primary text-center">
-          <h5 className="mb-4">Welcome to next.js!</h5>
-          <MyImage fname="image.jpg" size="300" />
-          <Link href="/other">
-            <button className="btn btn-primary">Go to other &gt;&gt;</button>
-          </Link>
+          <h5 className="mb-4">
+            {data != undefined ? data.message : "error..."}
+          </h5>
+          <table className="table bg-white">
+            <thead className="table-dark">
+              <tr>
+                <th>Name</th>
+                <th>Mail</th>
+                <th>Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data != undefined ? data.data.map((value, key) => (
+                <tr key={key}>
+                  <th>{value.name}</th>
+                  <th>{value.mail}</th>
+                  <th>{value.age}</th>
+                </tr>
+              )) : <tr><th></th><td>no data.</td></tr>}
+            </tbody>
+          </table>
         </div>
       </Layout>
     </div>
