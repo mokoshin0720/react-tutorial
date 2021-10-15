4e78ed2c32b16e82b66775a77ac9c943472d9bbc
mokoshin0720 <shinya.work0720@gmail.com>
2021-10-15 17:47:51 +0900

    [add]use an image file

--- book/super_basic_react_and_next/next_app/pages/index.js
+++ book/super_basic_react_and_next/next_app/pages/index.js
@@ -1,6 +1,8 @@
 import Header from "./header"
 import Link from "next/link"
+import Image from "next/image"
 import Layout from "../components/layout"
+import MyImage from "../components/image"
 
 export default function Home() {
   return (
@@ -8,6 +10,7 @@ export default function Home() {
       <Layout header="Next.js" title="Top page">
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">Welcome to next.js!</h5>
+          <MyImage fname="image.jpg" size="300" />
           <Link href="/other">
             <button className="btn btn-primary">Go to other &gt;&gt;</button>
           </Link>
