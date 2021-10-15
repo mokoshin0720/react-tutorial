4285e55018ef4df96c667794f5b0380b7b7e8bb2
mokoshin0720 <shinya.work0720@gmail.com>
2021-10-15 17:22:41 +0900

    [add]page link

--- book/super_basic_react_and_next/next_app/pages/index.js
+++ book/super_basic_react_and_next/next_app/pages/index.js
@@ -1,39 +1,21 @@
 import Header from "./header"
+import Link from "next/link"
 
 export default function Home() {
-  let title = "Next.js page"
-  let message = "React Next.js sample page"
+  const title = "Index"
 
   return (
     <div>
       <Header title={title}></Header>
 
-      <style jsx>{`
-        h1 {
-          text-align: center;
-        }
-
-        h2 {
-          text-align: center;
-          margin: 0px 5px;
-          color: #aad;
-          font-size: 36pt;
-          font-weight: bold;
-        }
-
-        p {
-          text-align: left;
-          margin: 0px 5px;
-          color: blue;
-          font-size: 18pt;
-        }
-      `}</style>
-
-      <h1 className="bg-primary text-white display-4">React</h1>
+      <h1 className="bg-primary text-white px-3 display-4">React</h1>
       <div className="container">
-        <h2 className="my-3 subtitle">{title}</h2>
-        <div className="alert alert-primary text-center">
-          <p className="h5">{message}.</p>
+        <h3 className="my-3 text-primary text-center">{title}</h3>
+        <div className="card p-3 text-center">
+          <p>これはページ移動のサンプルです</p>
+          <Link href="/other">
+            <a>Go to Other page &gt;&gt;</a>
+          </Link>
         </div>
       </div>
     </div>
