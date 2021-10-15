0f9b265975d27a4dc518e672199a83e3588dbe00
mokoshin0720 <shinya.work0720@gmail.com>
2021-10-15 18:45:32 +0900

    [add]use api as state

--- book/super_basic_react_and_next/next_app/pages/index.js
+++ book/super_basic_react_and_next/next_app/pages/index.js
@@ -1,35 +1,24 @@
 import Layout from "../components/layout"
 import useSWR from "swr"
+import { useState } from "react"
 
 export default function Home() {
+  const [address, setAddress] = useState("/api/hello")
   const func = (...args) => fetch(...args).then(res => res.json())
-  const {data} = useSWR('/data.json', func)
+  const {data, err} = useSWR(address, func)
+
+  const onChange = (e) => {
+    setAddress("/api/hello?id=" + e.target.value)
+  }
 
   return (
     <div>
       <Layout header="Next.js" title="Top page">
         <div className="alert alert-primary text-center">
           <h5 className="mb-4">
-            {data != undefined ? data.message : "error..."}
+            {JSON.stringify(data)}
           </h5>
-          <table className="table bg-white">
-            <thead className="table-dark">
-              <tr>
-                <th>Name</th>
-                <th>Mail</th>
-                <th>Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data != undefined ? data.data.map((value, key) => (
-                <tr key={key}>
-                  <th>{value.name}</th>
-                  <th>{value.mail}</th>
-                  <th>{value.age}</th>
-                </tr>
-              )) : <tr><th></th><td>no data.</td></tr>}
-            </tbody>
-          </table>
+          <input type="number" className="form-control" onChange={onChange} />
         </div>
       </Layout>
     </div>
